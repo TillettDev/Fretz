3c1b50112b937d31c1c39f71f793a2730cc25240
Jack T <jack@tillett.dev>
2022-07-17 22:01:34 +0100

    Changed SCSS for fret strings and started on adding notes as ID's to divs

--- main.js
+++ main.js
@@ -8,25 +8,33 @@ var className;
 
 var fret = document.createElement("div");
 
-var notes = ["C", "C♯", "D♭", "D", "D♯", "E♭", "E", "F", "F♯", "G♭", "G", "G♯"]
+// var notes = ["C", "C♯", "D♭", "D", "D♯", "E♭", "E", "F", "F♯", "G♭", "G", "G♯"]
+
+// var notes = ["F", "F♯", "G", "G♯", "A♭", "A", "A♯", "B♭", "B", "C", "C♯", "D♭", "D", "D♯", "E♭", "E"];
+
+var notes = ["F", "F♯", "G", "G♯", "A", "A♯", "B", "C", "C♯", "D", "D♯", "E"];
+
+var noteCount = 0;
+var offset = 4
 
 while (counter < 72){
 
-
-
-    if ([14,26,38,50,66,70].indexOf(counter) > -1){
+    if ([23, 26, 28, 30, 32, 47].indexOf(counter) > -1){
         className = "fret_mark"
     } else{ 
         className = ""; 
     }
 
-    if (counter == (counter)){
-        //work on nth term here - fix if
+    if (noteCount == 12){
+        noteCount = 5;
     }
+
     
     var fret = document.createElement("div");
     fret.className = className;
+    fret.setAttribute("id", notes[noteCount]);
     // fret.className = counter;
     fretboard.appendChild(fret);
     counter++;
+    noteCount++;
 }
