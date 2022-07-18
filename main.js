e883b7757f4c6a0d5581312577d552a9f3fef52c
Jack T <jack@tillett.dev>
2022-07-18 08:58:08 +0100

    Reworked nth term for strings, finished applying correct notes to divs - may rework this in future

--- main.js
+++ main.js
@@ -8,14 +8,8 @@ var className;
 
 var fret = document.createElement("div");
 
-// var notes = ["C", "C♯", "D♭", "D", "D♯", "E♭", "E", "F", "F♯", "G♭", "G", "G♯"]
-
-// var notes = ["F", "F♯", "G", "G♯", "A♭", "A", "A♯", "B♭", "B", "C", "C♯", "D♭", "D", "D♯", "E♭", "E"];
-
-var notes = ["F", "F♯", "G", "G♯", "A", "A♯", "B", "C", "C♯", "D", "D♯", "E"];
-
-var noteCount = 0;
-var offset = 4
+var notes = ["F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E"];
+var index = 0;
 
 while (counter < 72){
 
@@ -25,16 +19,40 @@ while (counter < 72){
         className = ""; 
     }
 
-    if (noteCount == 12){
-        noteCount = 5;
+
+    if (counter == 12){
+        index = 5;
+    } else if (counter == 24){
+        index = 10;
+    } else if (counter == 36){
+        index = 3;
+    } else if (counter == 48){
+        index = 7;
+    } else if (counter == 60){
+        index = 0;
+    }
+
+    if (index == 12){
+        index = 0;
     }
 
     
     var fret = document.createElement("div");
     fret.className = className;
-    fret.setAttribute("id", notes[noteCount]);
+    fret.setAttribute("id", notes[index]);
     // fret.className = counter;
     fretboard.appendChild(fret);
     counter++;
-    noteCount++;
+    index++;
 }
+
+
+console.log(Math.floor(Math.random() * 72));
+
+//generate random number, use that random number to target specific div inside .fretboard.
+//add class to that div to show it is selected
+
+
+//create function. If button clicked is = to value of random div id
+// generate new random div selected.
+
